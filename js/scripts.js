cf0eb9babb7cd3d98249b702fca01546f8edf419
edgar hernandez and james donlan <james@>
2017-12-11 09:38:49 -0800

    javascript constructor in place, along with user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,36 @@
 //business logic
-function Contact(first, last) {
-  this.firstName = first;
-  this.lastName = last;
-}
-
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
+function Location(name, landmark, note, date) {
+  this.placeName = name;
+  this.placeMark = landmark;
+  this.placeNote = note;
+  this.placeDate = date;
 }
 
 // user interface logic
 $(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
+  $("form#location").submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
+    var inputLocation = $("input#new-location").val();
+    var inputLandmark = $("input#new-landmark").val();
+    var inputNote = $("input#new-note").val();
+    var inputDate = $("#new-date").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var newLocation = new Location(inputLocation, inputLandmark, inputNote, inputDate);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    $("ul#destination").append("<li><span class='location'>" + newLocation.placeName + "</span></li>");
 
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
+    $(".location").last().click(function() {
+      $("#show-location").show();
+      $("#show-location h2").text(newLocation.placeName);
+      $(".place-mark").text(newLocation.placeMark);
+      $(".place-note").text(newLocation.placeNote);
+      $(".place-date").text(newLocation.placeDate);
     });
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
+    $("input#new-location").val("");
+    $("input#new-landmark").val("");
+    $("input#new-note").val("");
+    $("#new-date").val("")
   });
 });
